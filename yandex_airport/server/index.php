7270aa7bfd4b2e5655e1cf523c51e6e5b7946e5e
vit <vit134@mail.ru>
2017-03-17 19:12:26 +0300

    change yandex_airport widget

--- yandex_airport/server/index.php
+++ yandex_airport/server/index.php
@@ -1,32 +1,108 @@
 <?php
     //include('../../auth.php');
 
-    $domain = $_SERVER['HTTP_ORIGIN'];
+    /*$domain = $_SERVER['HTTP_ORIGIN'];
     $queryString = $_SERVER['QUERY_STRING'];
 
-    header("Access-Control-Allow-Origin:" . $domain );
+    header("Access-Control-Allow-Origin:" . $domain );*/
 
 
-    $response_arr = new stdClass();
-    $date = new DateTime();
+    $response = array();
+    $date = date('Y-m-d');
+    $nowDate = date('Y-m-d H:i:s');
+    $page = 1;
+    $pageCount= 0;
+
+    //$url = "apikey=de81013f-6678-44d3-84e6-dcf819a7182d&format=json&station=SVO&lang=ru&date=2017-03-17&transport_types=plane&event=arrival&system=iata";
+    $url = "https://api.rasp.yandex.net/v1.0/schedule/?";
+
+    $url .= "apikey=" . "de81013f-6678-44d3-84e6-dcf819a7182d";
+    $url .= "&format=" . "json";
+    $url .= "&station=" . "SVO";
+    $url .= "&lang=" . "ru";
+    $url .= "&date=" . $date;
+    $url .= "&transport_types=" . "plane";
+    $url .= "&system=" . "iata";
+    $url .= "&event=" . "arrival";
+    $url .= "&page=" . $page;
 
 
 
-    //$url = "https://kudago.com/public-api/v1.2/events?".$queryString;
-    $url = "http://api.openweathermap.org/data/2.5/forecast/daily?".$queryString;
+    $result = file_get_contents($url);
 
+    $resArr = json_decode($result);
 
-    $result = @file_get_contents($url) or
-        $result = FALSE;
+    $count = 0;
 
-    if ($result != '') {
-        $response_arr->status = 'success';
-        $response_arr->result = json_decode($result, true);
-    } else if ($result == FALSE) {
-       $response_arr->status = 'error';
+    foreach ($resArr->schedule as $key => $value) {
+        $response[] = $value;
     }
 
+    $response['count'] = $count + count($resArr->schedule);
 
-    echo json_encode($response_arr);
+    $pageCount = $resArr->pagination->page_count;
 
+
+    $ch = curl_init();
+    for ($i = 0; $i < $pageCount; $i += 1) {
+        $page = $i + 2;
+
+        curl_setopt_array($ch, array(
+            CURLOPT_URL => $url,
+            CURLOPT_VERBOSE => True,
+            CURLOPT_RETURNTRANSFER => True,
+        ));
+        $resp = json_decode(curl_exec($ch));
+
+        foreach ($resp->schedule as $key => $value) {
+            $response[] = $value;
+        }
+
+        $response['count'] += count($resp->schedule);
+        //$response[$page] = json_decode($resp);
+
+    }
+    curl_close($ch);
+
+   /*for ($i = 0; 2 > i; ++$i) {
+        $page = $i + 2;
+
+        $result = file_get_contents($url);
+
+        $resArr2 = json_decode($result);
+
+        $response[$i + 2] = $resArr2;
+
+        foreach ($resArr2->schedule as $key => $value) {
+            $response[$value->terminal][] = array(
+                'time' => $value->arrival,
+                'flightNumber' => $value->thread->number,
+                'flighttitle' => $value->thread->title,
+                'alreadyArrival' => $already
+            );
+        }
+    }*/
+
+
+    /*foreach ($resArr->schedule as $key => $value) {
+
+        $timeArrival = date($value->arrival);
+        $already = TRUE;
+
+        if ($nowDate > $timeArrival) {
+            $already = FALSE;
+        }
+
+        $response[$value->terminal][] = array(
+            'time' => $value->arrival,
+            'flightNumber' => $value->thread->number,
+            'flighttitle' => $value->thread->title,
+            'alreadyArrival' => $already
+        );
+
+    }*/
+
+    /*$response['nowTime'] = $nowDate;
+    $response['full'] = json_decode($result);*/
+    echo json_encode($response);
 ?>
