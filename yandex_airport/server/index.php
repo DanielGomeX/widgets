ba6ff3c1b00398436126d5728d2286d95eeb6b45
vit134 <vit134@mail.ru>
2017-03-19 17:40:14 +0300

    change yandex_airort widget

--- yandex_airport/server/index.php
+++ yandex_airport/server/index.php
@@ -6,102 +6,94 @@
 
     header("Access-Control-Allow-Origin:" . $domain );*/
 
-
+    //echo phpinfo();
     $response = array();
     $date = date('Y-m-d');
     $nowDate = date('Y-m-d H:i:s');
     $page = 1;
-    $pageCount= 0;
+    $pageCount= 0;    
 
-    //$url = "apikey=de81013f-6678-44d3-84e6-dcf819a7182d&format=json&station=SVO&lang=ru&date=2017-03-17&transport_types=plane&event=arrival&system=iata";
-    $url = "https://api.rasp.yandex.net/v1.0/schedule/?";
+    function setUrl ($page = 1) {
+        global $date;
 
-    $url .= "apikey=" . "de81013f-6678-44d3-84e6-dcf819a7182d";
-    $url .= "&format=" . "json";
-    $url .= "&station=" . "SVO";
-    $url .= "&lang=" . "ru";
-    $url .= "&date=" . $date;
-    $url .= "&transport_types=" . "plane";
-    $url .= "&system=" . "iata";
-    $url .= "&event=" . "arrival";
-    $url .= "&page=" . $page;
+        $url = "https://api.rasp.yandex.net/v1.0/schedule/?";
+        $query = array(
+            'apikey' => 'de81013f-6678-44d3-84e6-dcf819a7182d',
+            'format' => 'json',
+            'station' => 'svo',
+            'lang' => 'ru',
+            'date' => $date,
+            'transport_types' => 'plane',
+            'event' => 'arrival',
+            'system' => 'iata',
+            'page' => $page
+        );
 
+         return $url .= http_build_query($query);
+    }
 
-
-    $result = file_get_contents($url);
-
+    $result = file_get_contents(setUrl());
     $resArr = json_decode($result);
-
     $count = 0;
 
     foreach ($resArr->schedule as $key => $value) {
-        $response[] = $value;
-    }
 
-    $response['count'] = $count + count($resArr->schedule);
+        $timeArrival = date($value->arrival);
+        $already = TRUE;
+
+        if ($nowDate < $timeArrival) {
+
+            //$flightTime = date($value->arrival);
+            $flightTime = new DateTime($value->arrival);
+            
+            $response[$value->terminal][] = array(
+                'nowTime' => $nowDate,
+                'time' => $flightTime->format('H:i'),
+                'flightNumber' => $value->thread->number,
+                'flighttitle' => $value->thread->title
+            );
+            //$response['count'] = ++$count;
+        }
+
+    }
 
     $pageCount = $resArr->pagination->page_count;
 
-
     $ch = curl_init();
-    for ($i = 0; $i < $pageCount; $i += 1) {
-        $page = $i + 2;
-
+    for ($i = 2; $i <= $pageCount; $i += 1) {
+        
         curl_setopt_array($ch, array(
-            CURLOPT_URL => $url,
+            CURLOPT_URL => setUrl($i),
             CURLOPT_VERBOSE => True,
             CURLOPT_RETURNTRANSFER => True,
         ));
         $resp = json_decode(curl_exec($ch));
 
         foreach ($resp->schedule as $key => $value) {
-            $response[] = $value;
-        }
 
-        $response['count'] += count($resp->schedule);
+            $timeArrival = date($value->arrival);
+            $already = TRUE;
+
+            if ($nowDate < $timeArrival) {
+
+                $flightTime = new DateTime($value->arrival);
+
+                $response[$value->terminal][] = array(
+                    'nowTime' => $nowDate,
+                    'time' => $flightTime->format('H:i'),
+                    'flightNumber' => $value->thread->number,
+                    'flighttitle' => $value->thread->title
+                );
+
+                //$response['count'] = ++$count;
+            }
+            
+        }
 
     }
     curl_close($ch);
 
-   /*for ($i = 0; 2 > i; ++$i) {
-        $page = $i + 2;
-
-        $result = file_get_contents($url);
-
-        $resArr2 = json_decode($result);
-
-        $response[$i + 2] = $resArr2;
-
-        foreach ($resArr2->schedule as $key => $value) {
-            $response[$value->terminal][] = array(
-                'time' => $value->arrival,
-                'flightNumber' => $value->thread->number,
-                'flighttitle' => $value->thread->title,
-                'alreadyArrival' => $already
-            );
-        }
-    }*/
-
-
-    /*foreach ($resArr->schedule as $key => $value) {
-
-        $timeArrival = date($value->arrival);
-        $already = TRUE;
-
-        if ($nowDate > $timeArrival) {
-            $already = FALSE;
-        }
-
-        $response[$value->terminal][] = array(
-            'time' => $value->arrival,
-            'flightNumber' => $value->thread->number,
-            'flighttitle' => $value->thread->title,
-            'alreadyArrival' => $already
-        );
-
-    }*/
-
-    /*$response['nowTime'] = $nowDate;
-    $response['full'] = json_decode($result);*/
+    $response['length'] = count($response);
+    ksort($response);
     echo json_encode($response);
 ?>
