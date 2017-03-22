e5ce07e1d3a438316723dab392f51509ede495c2
vit <vit134@mail.ru>
2017-03-22 19:07:35 +0300

    add new widget "news"

---
+++ buisness_news/server/index.php
@@ -0,0 +1,43 @@
+<?php
+
+    $domain = $_SERVER['HTTP_ORIGIN'];
+    $queryString = $_SERVER['QUERY_STRING'];
+
+    header("Access-Control-Allow-Origin:" . $domain );
+
+    date_default_timezone_set('Europe/Moscow');
+
+    $response = array();
+
+
+    $result = file_get_contents('http://static.feed.rbc.ru/rbc/logical/footer/news.rss');
+
+    $resArr = json_decode($result);
+
+
+    $news = simplexml_load_file('http://static.feed.rbc.ru/rbc/logical/footer/news.rss', null, LIBXML_NOCDATA);
+
+    foreach ($news->channel->item as $key => $value) {
+
+        $response['news'][] = array(
+            'title' => (string) $value->title,
+            'description' => (string) $value->description,
+            'author' => (string) $value->author,
+            'pubDate' => (string) $value->pubDate
+        );
+
+    }
+
+    $rate = file_get_contents('https://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+%22USDRUB,EURRUB%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys');
+
+    $rate = json_decode($rate);
+
+    foreach ($rate->query->results->rate as $key => $value) {
+        $response['rate'][str_replace("/RUB", "", $value->Name)] = round($value->Rate, 2);
+    }
+
+    //$response['rate'] = json_decode($rate);
+
+    echo json_encode($response);
+
+?>
