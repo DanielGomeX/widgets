0fc69522b873b361175979b1b6a0beffee18123a
vit <vit134@mail.ru>
2017-03-28 19:11:57 +0300

    add new widget - sport_news? chenge image_slider

--- rbc_news/server/index.php
+++ rbc_news/server/index.php
@@ -8,7 +8,7 @@
     date_default_timezone_set('Europe/Moscow');
 
     $response = array();
-
+    $currentDate = date('d/m/Y');
 
     $result = file_get_contents('http://static.feed.rbc.ru/rbc/logical/footer/news.rss');
 
@@ -39,7 +39,18 @@
 
     }
 
-    $rate = file_get_contents('https://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+%22USDRUB,EURRUB%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys');
+    $rate = simplexml_load_file('http://www.cbr.ru/scripts/XML_daily.asp?date_req=' . $currentDate, null, LIBXML_NOCDATA);
+
+    foreach ($rate->Valute as $key => $value) {
+        if ($value->CharCode == 'EUR') {
+            $response['rate']['EUR'] = (string) substr($value->Value, 0, 5);
+        }
+
+        if ($value->CharCode == 'USD') {
+            $response['rate']['USD'] = (string) substr($value->Value, 0, 5);
+        }
+    }
+    /*$rate = file_get_contents('https://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+%22USDRUB,EURRUB%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys');
 
     $rate = json_decode($rate);
 
@@ -47,7 +58,7 @@
         $response['rate'][str_replace("/RUB", "", $value->Name)] = round($value->Rate, 2);
     }
 
-    $response['original'] = json_decode(json_encode($news));
+    $response['original'] = json_decode(json_encode($news));*/
 
     echo json_encode($response);
 
