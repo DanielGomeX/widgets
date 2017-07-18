4176fc37f72693be36c8820a0aa85c68a43eab33
Vitaliy Andryushkov <vandryushkov@rbc.ru>
2017-07-18 18:52:59 +0300

    change

--- parserUI/index.php
+++ parserUI/index.php
@@ -3,27 +3,9 @@
 <head>
     <title>parser</title>
     <script  src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-    <style type="text/css">
-        html,
-        body {
-            width: 100%;
-            height: 100%;
-            margin: 0;
-        }
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <script type="text/javascript" src="js/main.js"></script>
 
-        .content {
-            position: absolute;
-            top: 50%;
-            margin-top: -250px;
-            left: 50%;
-            margin-left: -250px;
-            width: 500px;
-            background-color: #eee;
-            min-height: 500px;
-            box-sizing: border-box;
-            padding: 30px;
-        }
-    </style>
 </head>
 <body>
     <div class="content">
@@ -34,37 +16,5 @@
             Осталось страниц <span class="ost-page"></span>
         </div>
     </div>
-    <script type="text/javascript">
-        $(document).ready(function() {
-            $.ajax({
-                url: 'prod/server/index.php',
-                success: function(data){
-                    console.log(data);
-                }
-            });
-
-            var interval = setInterval(function() {
-                $.ajax({
-                    url: 'prod/server/redirect.php',
-                    success: function(d){
-                        console.log('data--', JSON.parse(d));
-                        var data = JSON.parse(d)
-                        if (data.linkCount != 0) {
-                            console.log(data.linkCount - data.filesSave)
-                            $('.total-page').html(data.linkCount);
-                            $('.ost-page').html(data.linkCount - data.filesSave);
-
-                            if (data.linkCount == data.filesSave) {
-                                clearInterval(interval);
-                            }
-
-                        }
-                    }
-                });
-            }, 100)
-
-
-        })
-    </script>
 </body>
 </html>
---
+++ parserUI/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    /*$.ajax({
+        url: 'prod/server/index.php',
+        success: function(data){
+            console.log(data);
+        }
+    });
+
+    var interval = setInterval(function() {
+        $.ajax({
+            url: 'prod/server/redirect.php',
+            success: function(d){
+                console.log('data--', JSON.parse(d));
+                var data = JSON.parse(d)
+                if (data.linkCount != 0) {
+                    console.log(data.linkCount - data.filesSave)
+                    $('.total-page').html(data.linkCount);
+                    $('.ost-page').html(data.linkCount - data.filesSave);
+
+                    if (data.linkCount == data.filesSave) {
+                        clearInterval(interval);
+                    }
+
+                }
+            }
+        });
+    }, 100)
+
+*/
+})
