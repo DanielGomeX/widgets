2199d590cb41e7214adacdd6c17af3f733fee16e
Vitaliy Andryushkov <vandryushkov@rbc.ru>
2017-08-09 19:04:14 +0300

    change

--- attraction/dev/main.js
+++ attraction/dev/main.js
@@ -24,14 +24,42 @@ $(document).ready(function() {
 
             try {
                 console.log('attractions - ', JSON.parse(d));
-                //console.log('football data - ', d);
 
                 d = JSON.parse(d);
 
                 var data = d;
 
+                console.log('123',data.results[0].images[0].thumbnails['640x384']);
+
                 var template = Handlebars.compile( $('#template').html() );
                 $content.append( template(data) );
+
+
+                $('.slider').eq(0).on('init', function(slick){
+                    $('.js-block').eq(0).addClass('active');
+                    animateImg(0);
+                })
+
+                // Slick slider init
+                $('.slider').slick({
+                    arrows: false,
+                    dots: false,
+                    infinite: true,
+                    speed: 500,
+                    slidesToShow: 1,
+                    centerMode: true,
+                    variableWidth: true,
+                    draggable: false
+                });
+
+                $('.slider')
+                    .on('beforeChange', function(event, slick, currentSlide, nextSlide){
+                        $('.slick-list').addClass('do-transition')
+                    })
+                    .on('afterChange', function(){
+                        $('.slick-list').removeClass('do-transition')
+                    });
+
 
 
                 /*$preloader.hide();
@@ -49,5 +77,39 @@ $(document).ready(function() {
             $preloader.addClass('error')
         }
     })
+
+    function animateImg(blockIndex) {
+        var block = $('.js-block').eq(blockIndex);
+        var image = block.find('img')
+        var imageCount = image.length;
+
+        var slider = block.find('.slider');
+        var i = 1;
+
+        var timerId = setTimeout(function tick() {
+            if (i < imageCount) {
+                slider.slick('slickNext');
+                i++;
+            } else {
+                i = 0;
+                $(window).trigger('imageAnimateFinish');
+                clearTimeout(timerId);
+                return;
+            }
+
+            timerId = setTimeout(tick, 2000);
+        }, 2000);
+
+    }
+
+    $(window).on('imageAnimateFinish', function() {
+        var activeIndex = $('.js-block.active').index();
+        $('.js-block.active').removeClass('active');
+
+        $('.js-block').eq(activeIndex + 1).addClass('active');
+        animateImg(activeIndex + 1);
+    });
+
+
 
 })
--- attraction/prod/server/index.php
+++ attraction/prod/server/index.php
@@ -8,7 +8,9 @@
     $url = "https://kudago.com/public-api/v1.3/places/?";
 
     $params = array(
-        'fields' => 'title,address,images,description',
+        'fields' => 'short_title,address,images,description,categories,-online-shopping',
+        'location' => 'msk',
+        'expand' => 'images',
         'categories' => 'attractions',
         'text_format' => 'text'
     );
